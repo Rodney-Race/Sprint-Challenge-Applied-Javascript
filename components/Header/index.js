6a1502fd1a2b8de6a174137dc0e54858e143932f
Rodney-Race <rodneyrace@gmail.com>
2019-11-08 21:21:23 -0500

    1st commit

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,30 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+    // Elements
+function Header() {
+    const header = document.createElement('div');
+    const date = document.createElement('span');
+    const h1 = document.createElement('h1');
+    const temp = document.createElement('span');
+    const today = new date();
+
+    spanDate.textContent = today;
+    h1.textContent = 'Lambda Times';
+    spanTemp.textContent = '98°';
+
+    // styles
+    header.classList.add('header');
+    date.classList.add('date');
+    date.classList.add('temp');
+
+    //structure
+    header.appendChild(spanDate);
+    header.appendChild(h1);
+    header.appendChild(spanTemp);
+
+    return header;
+}
+
+const headerContainer = document.querySelector('.header-container');
+headerContainer.appendChild(header());
